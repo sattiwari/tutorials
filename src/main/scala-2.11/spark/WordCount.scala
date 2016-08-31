dd9275503daa5a4a98de31e877cea1da89d9c7d3
Satendra Tiwari <sattiwari18@gmail.com>
2016-08-31 09:04:47 +0700

    removed duplicate code

--- src/main/scala-2.11/spark/WordCount.scala
+++ src/main/scala-2.11/spark/WordCount.scala
@@ -1,15 +1,5 @@
 package tutorials.spark
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-
-trait SparkJob {
-  // Create scala spark context
-  val conf = new SparkConf().setMaster("local").setAppName("My App")
-  val sc = new SparkContext(conf)
-}
-
-
 object WordCount extends App with SparkJob{
   // Load our input data
   val input = sc.textFile("/tmp/README.md")
