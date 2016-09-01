8a1870e590fc160b3947b0ddf22e76857bf84ca8
Satendra Tiwari <sattiwari18@gmail.com>
2016-09-01 14:20:15 +0700

    TCP Server using Akka IO

--- src/main/scala-2.11/akka/AkkaIOExample.scala
+++ src/main/scala-2.11/akka/AkkaIOExample.scala
@@ -1,61 +1,45 @@
 package akka
 
-import akka.io.{IO, Tcp}
-import akka.actor.{ActorRef, Actor}
-import akka.io.Tcp._
 import java.net.InetSocketAddress
-import akka.util.ByteString
-import akka.io.Tcp.Connected
-import akka.io.Tcp.Register
-import akka.io.Tcp.Connect
-import akka.io.Tcp.CommandFailed
 
-/*
-Source - http://doc.akka.io/docs/akka/snapshot/scala/io-tcp.html
- */
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.io.Tcp.{Connected, Register, _}
+import akka.io.{IO, Tcp}
 
-//class Client(remote: InetSocketAddress, listener: ActorRef) extends Actor{
-//  IO(Tcp) ! Connect(remote)
-//
-//  def receive = {
-//    case CommandFailed(_: Connect) =>
-//      listener ! "connect failed"
-//      context.stop(self)
-//    case c @ Connected(remote, local) =>
-//      listener ! c
-//      val connection = sender()
-//      connection ! Register(self)
-//      context become {
-//        case data: ByteString =>
-//          connection ! Write(data)
-//        case CommandFailed(w: Write) =>
-//          listener ! "Write failed"
-//        case Received(data) =>
-//          listener ! data
-//        case "close" =>
-//          connection ! Close
-//        case _: ConnectionClosed =>
-//          listener ! "connection closed"
-//          context stop self
-//      }
-//  }
-//}
-//
-//class Server extends Actor {
-//  IO(Tcp) ! Bind(self, new InetSocketAddress("localhost", 0))
-//
-//  def receive = {
-//    case b @ Bound(localAddress) => println("binding successful")
-//    case CommandFailed(_: Bind) => context stop self
-//    case c @ Connected(remote, local) =>
-//
-//
-//  }
-//}
-//
-//class SimplisticHandler extends Actor {
-//  def receive = {
-//    case Received(data) => sender ! Write(data)
-//    case PeerClosed     => context stop self
-//  }
-//}
+import scala.io.StdIn._
+
+object AkkaIOExample extends App {
+  val system = ActorSystem("system")
+  val address = new InetSocketAddress("localhost", 11111)
+  val listener = system.actorOf(Listener.props(address), "listener")
+  readLine("press any key to exit")
+  system.shutdown()
+}
+
+object Listener {
+  def props(address: InetSocketAddress) = Props(new Listener(address))
+}
+
+class Listener(address: InetSocketAddress) extends Actor {
+  import context.system
+
+  IO(Tcp) ! Bind(self, address)
+
+  def receive = {
+    case Connected(remote, _) =>
+      sender ! Register(context.actorOf(ConnectionHandler.props(remote)))
+  }
+}
+
+object ConnectionHandler {
+  def props(address: InetSocketAddress) = Props(new ConnectionHandler(address))
+}
+
+class ConnectionHandler(address: InetSocketAddress) extends Actor {
+  def receive = {
+    case Received(data) =>
+      val text = data.utf8String.trim
+      if(text == "stop") context.stop(self)
+      else sender ! Write(data)
+  }
+}
