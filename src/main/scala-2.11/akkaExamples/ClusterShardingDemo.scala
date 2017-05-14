583249f393856f80007828766c2a35cce3789492
Satendra Tiwari <sattiwari18@gmail.com>
2017-05-14 22:59:21 +0700

    wip

---
+++ src/main/scala-2.11/akkaExamples/ClusterShardingDemo.scala
@@ -0,0 +1,75 @@
+package akkaExamples
+
+import akka.actor.{ActorRef, ActorSystem, Props, ReceiveTimeout}
+import akka.cluster.sharding.{ClusterSharding, ClusterShardingSettings, ShardRegion}
+import akka.persistence.PersistentActor
+
+import scala.concurrent.duration._
+
+case object Increment
+case object Decrement
+final case class Get(counterId: Long)
+final case class EntityEnvelope(id: Long, payload: Any)
+
+case object Stop
+final case class CounterChanged(delta: Int)
+
+class Counter extends PersistentActor {
+  import akka.cluster.sharding.ShardRegion.Passivate
+
+  context.setReceiveTimeout(120.seconds)
+
+  // self.path.name is the entity identifier (utf-8 URL-encoded)
+  override def persistenceId: String = "Counter-" + self.path.name
+
+  var count = 0
+
+  def updateState(event: CounterChanged): Unit =
+    count += event.delta
+
+  override def receiveRecover: Receive = {
+    case evt: CounterChanged ⇒ updateState(evt)
+  }
+
+  override def receiveCommand: Receive = {
+    case Increment      ⇒ persist(CounterChanged(+1))(updateState)
+    case Decrement      ⇒ persist(CounterChanged(-1))(updateState)
+    case Get(_)         ⇒ sender() ! count
+    case ReceiveTimeout ⇒ context.parent ! Passivate(stopMessage = Stop)
+    case Stop           ⇒ context.stop(self)
+  }
+}
+
+
+object ClusterShardingDemo {
+
+  val system = ActorSystem()
+
+  val extractEntityId: ShardRegion.ExtractEntityId = {
+    case EntityEnvelope(id, payload) ⇒ (id.toString, payload)
+    case msg @ Get(id)               ⇒ (id.toString, msg)
+  }
+
+  val numberOfShards = 100
+
+  val extractShardId: ShardRegion.ExtractShardId = {
+    case EntityEnvelope(id, _) ⇒ (id % numberOfShards).toString
+    case Get(id)               ⇒ (id % numberOfShards).toString
+  }
+
+  val counterRegion: ActorRef = ClusterSharding(system).start(
+    typeName = "Counter",
+    entityProps = Props[Counter],
+    settings = ClusterShardingSettings(system),
+    extractEntityId = extractEntityId,
+    extractShardId = extractShardId)
+
+  val counterRegion: ActorRef = ClusterSharding(system).shardRegion("Counter")
+  counterRegion ! Get(123)
+  expectMsg(0)
+
+  counterRegion ! EntityEnvelope(123, Increment)
+  counterRegion ! Get(123)
+  expectMsg(1)
+
+}
